7834abc1221bcc494f6863cae08a437668651f9e
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2020-06-23 21:24:02 -0400

    subiendo cambios

---
+++ PromYourSelf/Migrations/20200624003921_cambiando_tipo_dato_foto_empleado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PromYourSelf.Migrations
+{
+    public partial class cambiando_tipo_dato_foto_empleado : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Foto",
+                table: "Empleados",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Negocios",
+                keyColumn: "NegocioID",
+                keyValue: 1,
+                columns: new[] { "FechaCreacion", "FechaModificacion" },
+                values: new object[] { new DateTime(2020, 6, 23, 20, 39, 21, 798, DateTimeKind.Local).AddTicks(3846), new DateTime(2020, 6, 23, 20, 39, 21, 798, DateTimeKind.Local).AddTicks(9455) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Foto",
+                table: "Empleados",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Negocios",
+                keyColumn: "NegocioID",
+                keyValue: 1,
+                columns: new[] { "FechaCreacion", "FechaModificacion" },
+                values: new object[] { new DateTime(2020, 6, 23, 20, 8, 36, 581, DateTimeKind.Local).AddTicks(5490), new DateTime(2020, 6, 23, 20, 8, 36, 582, DateTimeKind.Local).AddTicks(3598) });
+        }
+    }
+}
---
+++ PromYourSelf/Utils/FileUtil.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace PromYourSelf.Utils
+{
+	public static class FileUtil
+	{
+		public static async Task<string> imageToBase64(IFormFile foto)
+		{
+			MemoryStream memoryStream = new MemoryStream();
+			await foto.CopyToAsync(memoryStream);
+			using (var image = Image.FromStream(memoryStream)) {
+				using (MemoryStream m = new MemoryStream())
+				{
+					image.Save(m, image.RawFormat); byte[] imageBytes = m.ToArray(); // Convert byte[] to Base64 String 
+					string base64String = Convert.ToBase64String(imageBytes); 
+					return base64String;
+				}
+			}
+		}
+	}
+		
+}
