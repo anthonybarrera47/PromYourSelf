dded9994230e3eb1b3e65dd0f7eca9d52f23df54
anthonybarrera47 <anthony_barrera@ucne.edu.do>
2020-08-13 19:52:40 -0400

    Cambios

--- PromYourSelf/Controllers/PagosController.cs
+++ PromYourSelf/Controllers/PagosController.cs
@@ -85,7 +85,7 @@ namespace PromYourSelf.Controllers
         // GET: Pagos/Create
         public async Task<IActionResult> Create()
         {
-            ViewBag.TipoClasificacion = await _Repo.TiposClasficacion.GetListAsync(x => true);
+            ViewBag.Productos = await _Repo.Productos.GetListAsync(x => true);
             return View(new Pagos());
         }
 
