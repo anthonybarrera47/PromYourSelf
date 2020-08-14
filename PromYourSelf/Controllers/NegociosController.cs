82dec42efd33b3ad837b4aa9925cf9a7d0916b5c
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2020-08-13 23:36:54 -0400

    Detalle de negocio: mostrando productos y servicios. Copiando telefonos al Clipboard del dispositivo

--- PromYourSelf/Controllers/NegociosController.cs
+++ PromYourSelf/Controllers/NegociosController.cs
@@ -17,20 +17,20 @@ using ReflectionIT.Mvc.Paging;
 
 namespace PromYourSelf.Controllers
 {
-    public class NegociosController : BaseController
-    {
-        private readonly Contexto db;
-        private readonly UserManager<Usuarios> _userManager;
-        private readonly SignInManager<Usuarios> _signInManager;
-        private readonly IRepoWrapper _Repo;
-        public static List<Negocios> Lista;
-        public NegociosController(Contexto context, IRepoWrapper RepoNegocio, SignInManager<Usuarios> signInManager, UserManager<Usuarios> userManager)
-        {
-            db = context;
-            _Repo = RepoNegocio;
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
+	public class NegociosController : BaseController
+	{
+		private readonly Contexto db;
+		private readonly UserManager<Usuarios> _userManager;
+		private readonly SignInManager<Usuarios> _signInManager;
+		private readonly IRepoWrapper _Repo;
+		public static List<Negocios> Lista;
+		public NegociosController(Contexto context, IRepoWrapper RepoNegocio, SignInManager<Usuarios> signInManager, UserManager<Usuarios> userManager)
+		{
+			db = context;
+			_Repo = RepoNegocio;
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
 		// GET: GetNegocios
 		public async Task<IActionResult> GetNegocios(string filter, int page = 1, string sortExpression = "NombreComercial", int PageSize = 5)
 		{
@@ -42,6 +42,14 @@ namespace PromYourSelf.Controllers
 			return new JsonResult(JsonConvert.SerializeObject(Lista));
 		}
 
+
+		public async Task<IActionResult> GetProductosByNegocio(int negocioId, int page = 1, string sortExpression = "NombreComercial", int PageSize = 5)
+		{
+			List<Productos> lista = new List<Productos>();
+			lista = await _Repo.Productos.GetListAsync(x => x.NegocioID == negocioId);
+
+			return new JsonResult(JsonConvert.SerializeObject(lista));
+		}
 
 		// GET: s
 		public async Task<IActionResult> GetProductos(string filter, int page = 1, string sortExpression = "NombreComercial", int PageSize = 5)
@@ -58,44 +66,44 @@ namespace PromYourSelf.Controllers
 
 		// GET: Negocios
 		public async Task<IActionResult> Index(string filter, int page = 1, string sortExpression = "NombreComercial", int PageSize = 5)
-        {
-            if (!string.IsNullOrWhiteSpace(filter))
-                Lista = await _Repo.Negocios.GetListAsync(x => x.NombreComercial.ToUpper().Contains(filter.ToUpper()));
-            else
-                Lista = await _Repo.Negocios.GetListAsync(x => true);
+		{
+			if (!string.IsNullOrWhiteSpace(filter))
+				Lista = await _Repo.Negocios.GetListAsync(x => x.NombreComercial.ToUpper().Contains(filter.ToUpper()));
+			else
+				Lista = await _Repo.Negocios.GetListAsync(x => true);
 
-            var model = PagingList.Create(Lista, PageSize, page, sortExpression, "NombreComercial");
-            model.RouteValue = new RouteValueDictionary {
-                            { "filter", filter}
-            };
-            model.Action = "Index";
+			var model = PagingList.Create(Lista, PageSize, page, sortExpression, "NombreComercial");
+			model.RouteValue = new RouteValueDictionary {
+							{ "filter", filter}
+			};
+			model.Action = "Index";
 
-            return View(model);
-        }
+			return View(model);
+		}
 
-        // GET: Negocios/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Negocios/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var negocios = await _Repo.Negocios.FindAsync(id);
+			var negocios = await _Repo.Negocios.FindAsync(id);
 
-            if (negocios == null)
-            {
-                return NotFound();
-            }
+			if (negocios == null)
+			{
+				return NotFound();
+			}
 
-            return View(negocios);
-        }
+			return View(negocios);
+		}
 
 		// GET: Negocios/Details/5
 		public async Task<IActionResult> MiNegocio(int? id)
 		{
 			Negocios negocio = (await _Repo.Negocios.GetListAsync(m => m.UsuarioID == id)).FirstOrDefault();
-						
+
 
 			if (negocio == null)
 			{
@@ -107,113 +115,113 @@ namespace PromYourSelf.Controllers
 
 		// GET: Negocios/Create
 		public IActionResult Create()
-        {
-            return View(new Negocios());
-        }
+		{
+			return View(new Negocios());
+		}
 
-        // POST: Negocios/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Negocios negocios)
-        {
-            if (ModelState.IsValid)
-            {
-                int UsuarioId = User.GetUserID().ToInt();
-                negocios.UsuarioID = UsuarioId;
+		// POST: Negocios/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(Negocios negocios)
+		{
+			if (ModelState.IsValid)
+			{
+				int UsuarioId = User.GetUserID().ToInt();
+				negocios.UsuarioID = UsuarioId;
 
-                if (await _Repo.Negocios.SaveAsync(negocios))
-                {
-                    var Usuario = await _Repo.Usuarios.FindAsync(UsuarioId);
-                    Usuario.Posicion = Posicion.Administrador.GetDescription();
-                    await _Repo.Usuarios.UpdateClaimsUser(_signInManager, _userManager, Usuario);
-                }
-                
-                return RedirectToAction(nameof(Index));
-            }
-            return View(negocios);
-        }
+				if (await _Repo.Negocios.SaveAsync(negocios))
+				{
+					var Usuario = await _Repo.Usuarios.FindAsync(UsuarioId);
+					Usuario.Posicion = Posicion.Administrador.GetDescription();
+					await _Repo.Usuarios.UpdateClaimsUser(_signInManager, _userManager, Usuario);
+				}
 
-        // GET: Negocios/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+				return RedirectToAction(nameof(Index));
+			}
+			return View(negocios);
+		}
 
-            var negocios = await _Repo.Negocios.FindAsync(id);
-            if (negocios == null)
-            {
-                return NotFound();
-            }
-            return View(negocios);
-        }
+		// GET: Negocios/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Negocios/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Negocios negocios)
-        {
-            if (id != negocios.NegocioID)
-            {
-                return NotFound();
-            }
+			var negocios = await _Repo.Negocios.FindAsync(id);
+			if (negocios == null)
+			{
+				return NotFound();
+			}
+			return View(negocios);
+		}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    await _Repo.Negocios.ModifiedAsync(negocios);
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!NegociosExists(negocios.NegocioID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(negocios);
-        }
+		// POST: Negocios/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, Negocios negocios)
+		{
+			if (id != negocios.NegocioID)
+			{
+				return NotFound();
+			}
 
-        // GET: Negocios/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					await _Repo.Negocios.ModifiedAsync(negocios);
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!NegociosExists(negocios.NegocioID))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(negocios);
+		}
 
-            var negocios = await _Repo.Negocios.FindAsync(id);
-            if (negocios == null)
-            {
-                return NotFound();
-            }
+		// GET: Negocios/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            return View(negocios);
-        }
+			var negocios = await _Repo.Negocios.FindAsync(id);
+			if (negocios == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Negocios/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            await _Repo.Negocios.DeleteAsync(id);
-            return RedirectToAction(nameof(Index));
-        }
+			return View(negocios);
+		}
 
-        private bool NegociosExists(int id)
-        {
-            return _Repo.Negocios.Exists(id);
-        }
-    }
+		// POST: Negocios/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			await _Repo.Negocios.DeleteAsync(id);
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool NegociosExists(int id)
+		{
+			return _Repo.Negocios.Exists(id);
+		}
+	}
 }
