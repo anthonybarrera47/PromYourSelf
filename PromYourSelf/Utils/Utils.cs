0a7055473be38fc06031e9c4e9a6614508975d5b
anthonybarrera47 <anthony_barrera@ucne.edu.do>
2020-08-20 12:21:13 -0400

    Corrigiendo el seed de los usuarios

--- PromYourSelf/Utils/Utils.cs
+++ PromYourSelf/Utils/Utils.cs
@@ -16,177 +16,205 @@ using System.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using System.Net.Mail;
+using PromYourSelf.BLL;
+using Microsoft.AspNetCore.Identity;
+using PromYourSelf.Models;
 
 namespace PromYourSelf.Utils
 {
-	public static class Utils
-	{
+    public static class Utils
+    {
         public static async Task<string> ImageToBase64(IFormFile foto)
-		{
-			if (foto != null)
-			{
-				MemoryStream memoryStream = new MemoryStream();
-				await foto.CopyToAsync(memoryStream);
-				using (var image = Image.FromStream(memoryStream))
-				{
-					using (MemoryStream m = new MemoryStream())
-					{
-						image.Save(m, image.RawFormat); byte[] imageBytes = m.ToArray(); // Convert byte[] to Base64 String 
-						string base64String = Convert.ToBase64String(imageBytes);
-						return base64String;
-					}
-				}
-			}
-			else
-				return string.Empty;
-
-		}
-		public static string GetUserID(this ClaimsPrincipal principal)
-		{
-			if (principal == null)
-				throw new ArgumentNullException(nameof(principal));
-
-			return principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-		}
-
-		/// <summary>
-		/// Busca en el claimPrincipal el username del usuario
-		/// </summary>
-		/// <param name="principal"></param>
-		/// <returns></returns>
-		public static string GetUserName(this ClaimsPrincipal principal)
-		{
-			if (principal == null)
-				throw new ArgumentNullException(nameof(principal));
-			return principal.FindFirst(ClaimTypes.Name)?.Value;
-		}
-
-		public static string GetNombre(this ClaimsPrincipal principal)
-		{
-			if (principal == null)
-				throw new ArgumentNullException(nameof(principal));
-			return principal.FindFirst(c => c.Type == TypeClaims.Nombres.ToString("G"))?.Value;
-		}
-		public static string GetEmpresaID(this ClaimsPrincipal principal)
-		{
-			if (principal == null)
-				throw new ArgumentNullException(nameof(principal));
-			return principal.FindFirst(c => c.Type == TypeClaims.Empresa.ToString("G"))?.Value;
-		}
-		public static string GetPosicion(this ClaimsPrincipal principal)
-		{
-			if (principal == null)
-				throw new ArgumentNullException(nameof(principal));
-			return principal.FindFirst(c => c.Type == TypeClaims.Posicion.ToString("G"))?.Value;
-		}
-		public static string GetFoto(this ClaimsPrincipal principal)
-		{
-			if (principal == null)
-				throw new ArgumentNullException(nameof(principal));
-			return principal.FindFirst(c => c.Type == TypeClaims.Foto.ToString("G"))?.Value;
-		}
-
-		public static bool IsValidEmail(this string email)
-		{
-			if (string.IsNullOrWhiteSpace(email))
-				return false;
-
-			try
-			{
-				// Normalize the domain
-				email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
-									  RegexOptions.None, TimeSpan.FromMilliseconds(200));
-
-				// Examines the domain part of the email and normalizes it.
-				string DomainMapper(Match match)
-				{
-					// Use IdnMapping class to convert Unicode domain names.
-					var idn = new IdnMapping();
-
-					// Pull out and process domain name (throws ArgumentException on invalid)
-					var domainName = idn.GetAscii(match.Groups[2].Value);
-
-					return match.Groups[1].Value + domainName;
-				}
-			}
-			catch (RegexMatchTimeoutException e)
-			{
-				Console.WriteLine(e.Message);
-				return false;
-			}
-			catch// (ArgumentException e)
-			{
-				return false;
-			}
-
-			try
-			{
-				return Regex.IsMatch(email,
-					@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-					@"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
-					RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-			}
-			catch (RegexMatchTimeoutException)
-			{
-				return false;
-			}
-		}
-		public static PropertyInfo[] GetProperties(object obj)
-		{
-			return obj.GetType().GetProperties();
-		}
-
-		public static List<T> RawSqlQuery<T>(string query, Func<DbDataReader, T> map)
-		{
-			using (var context = new Contexto())
-			{
-				using (var command = context.Database.GetDbConnection().CreateCommand())
-				{
-					command.CommandText = query;
-					command.CommandType = CommandType.Text;
-
-					context.Database.OpenConnection();
-
-					using (var result = command.ExecuteReader())
-					{
-						var entities = new List<T>();
-
-						while (result.Read())
-						{
-							entities.Add(map(result));
-						}
-
-						return entities;
-					}
-				}
-			}
-		}
-		public static void SeedCuidades(MigrationBuilder migrationBuilder)
         {
-			//Utils.Utils.SeedCuidades(migrationBuilder);
-			string Path = Environment.CurrentDirectory;
-			string sql = File.ReadAllText($@"{Path}\Data\Ciudades.sql");
-			migrationBuilder.Sql(sql);
-		}
-		public static string GenerarToken(int longitud = 8)
-		{
+            if (foto != null)
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                await foto.CopyToAsync(memoryStream);
+                using (var image = Image.FromStream(memoryStream))
+                {
+                    using (MemoryStream m = new MemoryStream())
+                    {
+                        image.Save(m, image.RawFormat); byte[] imageBytes = m.ToArray(); // Convert byte[] to Base64 String 
+                        string base64String = Convert.ToBase64String(imageBytes);
+                        return base64String;
+                    }
+                }
+            }
+            else
+                return string.Empty;
+
+        }
+        public static string GetUserID(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            return principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+
+        /// <summary>
+        /// Busca en el claimPrincipal el username del usuario
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <returns></returns>
+        public static string GetUserName(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+            return principal.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static string GetNombre(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+            return principal.FindFirst(c => c.Type == TypeClaims.Nombres.ToString("G"))?.Value;
+        }
+        public static string GetEmpresaID(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+            return principal.FindFirst(c => c.Type == TypeClaims.Empresa.ToString("G"))?.Value;
+        }
+        public static string GetPosicion(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+            return principal.FindFirst(c => c.Type == TypeClaims.Posicion.ToString("G"))?.Value;
+        }
+        public static string GetFoto(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+            return principal.FindFirst(c => c.Type == TypeClaims.Foto.ToString("G"))?.Value;
+        }
+
+        public static bool IsValidEmail(this string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    var domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            catch// (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                    @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+        public static PropertyInfo[] GetProperties(object obj)
+        {
+            return obj.GetType().GetProperties();
+        }
+
+        public static List<T> RawSqlQuery<T>(string query, Func<DbDataReader, T> map)
+        {
+            using (var context = new Contexto())
+            {
+                using (var command = context.Database.GetDbConnection().CreateCommand())
+                {
+                    command.CommandText = query;
+                    command.CommandType = CommandType.Text;
+
+                    context.Database.OpenConnection();
+
+                    using (var result = command.ExecuteReader())
+                    {
+                        var entities = new List<T>();
+
+                        while (result.Read())
+                        {
+                            entities.Add(map(result));
+                        }
+
+                        return entities;
+                    }
+                }
+            }
+        }
+        public static void SeedCuidades(MigrationBuilder migrationBuilder)
+        {
+            //Utils.Utils.SeedCuidades(migrationBuilder);
+            string Path = Environment.CurrentDirectory;
+            string sql = File.ReadAllText($@"{Path}\Data\Ciudades.sql");
+            migrationBuilder.Sql(sql);
+        }
+        public static string GenerarToken(int longitud = 8)
+        {
             Guid miGuid = Guid.NewGuid();
-			string token = Convert.ToBase64String(miGuid.ToByteArray());
-			token = token.Replace("=", "").Replace("+", "");
+            string token = Convert.ToBase64String(miGuid.ToByteArray());
+            token = token.Replace("=", "").Replace("+", "");
             string str = token.Substring(0, longitud);
             return str;
-		}
-		public static async Task SendMail(MailMessage Mail)
+        }
+        public static async Task SendMail(MailMessage Mail)
         {
-			SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com")
-			{
-				Port = 587,
-				Credentials = new System.Net.NetworkCredential("proyectoaplicada2@gmail.com", "@P123456"),
-				EnableSsl = true
-			};
-			await SmtpServer.SendMailAsync(Mail);
-		}
-	}
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new System.Net.NetworkCredential("proyectoaplicada2@gmail.com", "@P123456"),
+                EnableSsl = true
+            };
+            await SmtpServer.SendMailAsync(Mail);
+        }
+        public static async Task SaveUser(UserManager<Usuarios> userManager, RoleManager<Roles> rolManager, Usuarios usuario)
+        {
+            var result = await userManager.CreateAsync(usuario, usuario.Password);
+            string posicion = usuario.Posicion == Posicion.Administrador.GetDescription() ? Posicion.Administrador.GetDescription() : Posicion.Normal.GetDescription();
+
+            if (result.Succeeded)
+            {
+                await userManager.AddClaimAsync(usuario, new Claim(TypeClaims.Nombres.ToString("G"), usuario.Nombres));
+                await userManager.AddClaimAsync(usuario, new Claim(TypeClaims.Posicion.ToString("G"), usuario.Posicion));
+                Roles rol = await rolManager.FindByNameAsync(posicion);
+                if (rol == null)
+                {
+                    rol = new Roles() { Name = posicion };
+                    var r = await rolManager.CreateAsync(rol);
+                    if (r.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(usuario, posicion);
+                    }
+                }else
+                    await userManager.AddToRoleAsync(usuario, posicion);
+
+                Console.WriteLine("Todo Bien.");
+            }
+
+        }
+    }
 
 
 }
